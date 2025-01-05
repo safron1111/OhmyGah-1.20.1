cc1bd6df9ba6cd3da65bb3fb316c41ce2642375c
safron1111 <testersignifigance@gmail.com>
2025-01-04 19:48:33 -0600

    Initial commit

---
+++ src/main/java/net/jeb/ohmygah/OhmygahClient.java
@@ -0,0 +1,10 @@
+package net.jeb.ohmygah;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class OhmygahClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/jeb/ohmygah/OhmygahDataGenerator.java
@@ -0,0 +1,11 @@
+package net.jeb.ohmygah;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class OhmygahDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
